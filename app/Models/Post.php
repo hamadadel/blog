fc965a0f5fa50ce1de10de01d8e9a3cdb8f6d870
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-12-16 15:18:37 +0200

    allow post model to relate to user model  by adding belongsTo method in the post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
     // protected $table = 'posts';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
